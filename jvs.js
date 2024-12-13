cc1d2a571865238fec19fd86b409476811d15a36
saulgn2007 <saulgonzaleznavarro2007@gmail.com>
2024-12-13 13:13:19 +0100

    Examen_Tema_4

---
+++ jvs.js
@@ -0,0 +1,21 @@
+document.getElementById('btn3').addEventListener('click', function mostrarmensaje(){
+    const container=document.createElement('div');
+    const note = document.getElementById('enviarMensaje').value;
+    container.textContent=note;
+    document.getElementById('conversacion').appendChild(container)
+});
+
+function ocultar(){
+    chat.classList.add('oculto')
+}
+
+function mostrar(){
+    chat.classList.remove('oculto')
+}
+
+document.getElementById('enviarMensaje').addEventListener('keydown', function comprobarEnter(event) {
+    if (event.key === 'Enter') {
+    event.preventDefault();
+    mostrarmensaje();
+    }
+});
